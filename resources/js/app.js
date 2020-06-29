7335032e08943da7de8692f455fe6af2fab45b49
AlexOG <alexx1708@gmail.com>
2020-06-28 18:20:59 -0600

    General Config to use vue n vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,8 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+import Vuetify from 'vuetify';
+Vue.use(Vuetify);
 
 /**
  * The following block of code may be used to automatically register your
@@ -16,10 +18,12 @@ window.Vue = require('vue');
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
+
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -27,6 +31,9 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+Vue.component('product-card', require('./components/ProductCardComponent.vue').default);
+
 const app = new Vue({
-    el: '#app',
+  el: '#app',
+  vuetify: new Vuetify()
 });
